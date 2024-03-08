b3517d0076ad227713899bdc3d9e495b212c2b20
AnanditaDev <ananditadev4@gmail.com>
2024-03-08 10:49:28 -0800

    function is added in index and script file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const age = 24;
+const name = 'Anandita'
+const dob = '01-01-1999'
+
+function getName(){
+    console.log("my name is " + name);
+}
+
+function isabove18(){
+    return age>=18;
+}
