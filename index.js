04451042a8f83ed5763bad9861d5734584c8d33d
AnanditaDev <ananditadev4@gmail.com>
2024-03-08 10:59:54 -0800

    change the variable name to name1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const age = 24;
-const name = 'Anandita'
+const name1 = 'Anandita'
 const dob = '01-01-1999'
 
 function getName(){
-    console.log("my name is " + name);
+    console.log("my name is " + name1);
 }
 
 function isabove18(){
